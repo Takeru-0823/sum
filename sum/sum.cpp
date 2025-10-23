772948b54d0b6ac4cff3484e600bbbe9ba4d4d95
buffa <buffaloes0534@icloud.com>
2025-10-23 14:43:59 +0900

    sum

--- sum/sum.cpp
+++ sum/sum.cpp
@@ -1,8 +1,15 @@
 ﻿#include <iostream>
+#include "calc.h"
 using namespace std;
 
 int main()
 {
-    
+    int x = 34;
+    int y = 5;
+    int answer = 0;
+
+    answer = sum(x, y);
+
+    cout << "答えは " << answer << endl;
 }
 
